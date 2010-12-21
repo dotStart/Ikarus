99909f763b85e96510bc19d933308f94a048c2bf
Johannes Donath <akkarin@develfusion.com>
2010-12-21 14:47:19 +0100

    Created IKARUSACP

---
+++ lib/system/IKARUSACP.class.php
@@ -0,0 +1,17 @@
+<?php
+// ikarus imports
+require_once(IKARUS_DIR.'lib/system/IKARUS.class.php');
+
+/**
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		com.develfusion.ikarus
+ * @subpackage	system
+ * @category	Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		1.0.0-0001
+ */
+class IKARUSACP extends IKARUS {
+	// This is just a placeholder
+}
+?>
