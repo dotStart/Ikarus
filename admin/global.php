9a3b9ed563fb03daf9f65d257b08d5fa4c2b6840
Johannes Donath <akkarin@develfusion.com>
2010-12-21 16:12:42 +0100

    Added global.php for acp

---
+++ admin/global.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		com.develfusion.ikarus
+ * @subpackage	system
+ * @category	Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		1.0.0-0001
+ */
+
+// defines
+define('REALTIVE_IKARUS_DIR', '../');
+
+// define package dirs
+$packageDirs = array(dirname(dirname(__FILE__)));
+
+// include IKARUS
+require_once(RELATIVE_IKARUS_DIR.'global.php');
+
+// start admin panel
+require_once(IKARUS_DIR.'lib/system/IKARUSACP.class.php');
+new IKARUSACP();
+?>
