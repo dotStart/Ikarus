82a9147397d9ac1b51fec6c4f53af1df8059d0ac
Johannes Donath <akkarin@develfusion.com>
2010-12-21 11:08:22 +0100

    Created placeholder for installer

---
+++ contrib/installer/install.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This exception will thrown if an system error occours
+ * Note: This is the red screen of death
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		com.develfusion.ikarus
+ * @subpackage	system
+ * @category	Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		1.0.0-0001
+ * @todo		Errr ... Implement?!
+ */
+?>
