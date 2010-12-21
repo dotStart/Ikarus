2fa31d4a2439c47cc66e002c21532efc2f923841
Johannes Donath <akkarin@develfusion.com>
2010-12-21 17:44:57 +0100

    Created CacheBuilder

---
+++ lib/system/cache/CacheBuilder.class.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Defines default methods for cache builders
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		com.develfusion.ikarus
+ * @subpackage	system
+ * @category	Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		1.0.0-0001
+ */
+interface CacheBuilder {
+
+	/**
+	 * Returnes the data for new cache file
+	 * @param	string	$file
+	 * @param	mixed	$minLifetime
+	 * @param	mixed	$maxLifetime
+	 * @return mixed
+	 */
+	public function getData($file, $minLifetime, $maxLifetime);
+}
+?>
