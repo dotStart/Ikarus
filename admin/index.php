ac6653beff0cc87fb0cfda3bdf2f9ef4a5feb76f
Johannes Donath <akkarin@develfusion.com>
2010-12-21 15:57:37 +0100

    Created index.php for admin area

---
+++ admin/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		com.develfusion.ikarus
+ * @subpackage	system
+ * @category	Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		1.0.0-0001
+ */
+
+require_once('./global.php');
+RequestHandler::handle(ArrayUtil::appendSuffix($packageDirs, 'lib/acp/'));
+?>
