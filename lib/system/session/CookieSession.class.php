c6379661086706c6442bc6c58d3daa80050319a9
Johannes Donath <akkarin@develfusion.com>
2010-12-22 14:59:26 +0100

    Implemented CookieSession

---
+++ lib/system/session/CookieSession.class.php
@@ -0,0 +1,25 @@
+<?php
+// ikarus imports
+require_once(IKARUS_DIR.'lib/system/session/SessionEditor.class.php');
+
+/**
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		com.develfusion.ikarus
+ * @subpackage	system
+ * @category	Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		1.0.0-0001
+ */
+class CookieSession extends SessionEditor {
+
+	/**
+	 * @see lib/data/DatabaseObject::handleData()
+	 */
+	protected function handleData($data) {
+		parent::handleData($data);
+
+		UserUtil::handleSessionArguments($this);
+	}
+}
+?>
