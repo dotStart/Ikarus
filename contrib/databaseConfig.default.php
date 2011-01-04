d3741f77c62e5b01763a74c7114b092200008038
Johannes Donath <akkarin@develfusion.com>
2011-01-04 21:19:45 +0100

    Created example configuration

---
+++ contrib/databaseConfig.default.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * IKARUS DATABASE CONFIGURATION
+ * Note: This is an example layout for database configurations
+ */
+$dbType = 'MySQL';
+$dbHostname = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbDatabase = 'ikarus';
+?>
