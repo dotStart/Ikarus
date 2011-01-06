5fdb9a564f7596fb7009e1dfef062947eed246b1
Johannes Donath <akkarin@develfusion.com>
2011-01-06 02:22:46 +0100

    Implemented Language

---
+++ lib/system/language/Language.class.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Represents a language row
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @package		com.develfusion.ikarus
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		1.0.0-0001
+ */
+class Language extends DatabaseObject {
+	
+	/**
+	 * Creates a new instance of type Language
+	 * @param	integer	$languageID
+	 * @param	array	$row
+	 */
+	public function __construct($languageID, $row = null) {
+		if ($languageID !== null) {
+			$sql = "SELECT
+					*
+				FROM
+					ikarus".IKARUS_N."_language
+				WHERE
+					languageID = ".$languageID;
+			$row = IKARUS::getDatabase()->getFirstRow($sql);
+		}
+		
+		parent::__construct($row);
+	}
+}
+?>
