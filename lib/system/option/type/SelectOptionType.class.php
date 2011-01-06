8e3d7c96592870730711698e6b221bd7cfe706c6
Johannes Donath <akkarin@develfusion.com>
2011-01-06 01:37:49 +0100

    Created select option type

---
+++ lib/system/option/type/SelectOptionType.class.php
@@ -0,0 +1,18 @@
+<?php
+// ikarus imports
+require_once(IKARUS_DIR.'lib/system/option/type/TextOptionType.class.php');
+
+/**
+ * Option type for string values with specified set of values
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @package		com.develfusion.ikarus
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		1.0.0-0001
+ */
+class SelectOptionType extends TextOptionType {
+	
+}
+?>
