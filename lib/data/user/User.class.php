55a828e0490745ae9fc3cd6e8295ddc122e5732b
Johannes Donath <akkarin@develfusion.com>
2011-01-13 15:28:04 +0100

    Created User.class.php

---
+++ lib/data/user/User.class.php
@@ -0,0 +1,36 @@
+<?php
+// ikarus imports
+require_once(IKARUS_DIR.'lib/data/DatabaseObject.class.php');
+
+/**
+ * Represents a user row
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @package		com.develfusion.ikarus
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		1.0.0-0001
+ */
+class User extends DatabaseObject {
+	
+	/**
+	 * Reads a row from database
+	 * @param	integer	$userID
+	 * @param	array	$row
+	 */
+	public function __construct($userID, $row = null) {
+		if ($userID !== null) {
+			$sql = "SELECT
+					*
+				FROM
+					ikarus".IKARUS_N."_user
+				WHERE
+					userID = ".$userID;
+			$row = IKARUS::getDatabase()->getFirstRow($sql);
+		}
+		
+		parent::__construct($row);
+	}
+}
+?>
