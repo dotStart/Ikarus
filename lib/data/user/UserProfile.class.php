ea6d3696cf57b627269643a143fb8b0616953bac
Johannes Donath <akkarin@develfusion.com>
2011-01-13 15:29:10 +0100

    Created UserProfile

---
+++ lib/data/user/UserProfile.class.php
@@ -0,0 +1,15 @@
+<?php
+// ikarus imports
+require_once(IKARUS_DIR.'lib/data/user/User.class.php');
+
+/**
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @package		com.develfusion.ikarus
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		1.0.0-0001
+ */
+class UserProfile extends User { }
+?>
