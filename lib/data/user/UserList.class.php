34524bc55e28b955bc69fd90fa45df5b9653978f
Johannes Donath <akkarin@develfusion.com>
2011-01-13 15:35:07 +0100

    Implemented UserList

---
+++ lib/data/user/UserList.class.php
@@ -0,0 +1,46 @@
+<?php
+// ikarus imports
+require_once(IKARUS_DIR.'lib/data/DatabaseObjectList.class.php');
+require_once(IKARUS_DIR.'lib/data/user/UserProfile.class.php');
+
+/**
+ * Manages a list of users
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @package		com.develfusion.ikarus
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		1.0.0-0001
+ */
+class UserList extends DatabaseObjectList {
+	
+	/**
+	 * Contains the name of the class that should used in list
+	 * @var string
+	 */
+	protected $listObject = 'UserProfile';
+	
+	/**
+	 * Creates a new instance of UserList
+	 * @param	integer	$limit
+	 * @param	integer	$offset
+	 * @param	string	$whereClouse
+	 */
+	public function __construct($limit = 0, $offset = 0, $whereClouse = null) {
+		$objectList = array();
+		$sql = "SELECT
+				*
+			FROM
+				ikarus".IKARUS_N."_user
+			".($whereClouse !== null ? "WHERE ".$whereClouse : "");
+		$result = IKARUS::getDatabase()->sendQuery($sql, $limit, $offset);
+		
+		while($row = IKARUS::getDatabase()->fetchArray($result)) {
+			$objectList[] = new $this->listObject(null, $row);
+		}
+		
+		parent::__construct($objectList);
+	}
+}
+?>
