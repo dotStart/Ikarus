1dd823128190e75f1dc0759e058e5cfd971e1869
Johannes Donath <akkarin@develfusion.com>
2011-05-08 01:37:34 +0200

    Added a new autoloader (WUHAHAHAHAHA)

--- lib/core.functions.php
+++ lib/core.functions.php
@@ -18,8 +18,42 @@ set_error_handler(array('IKARUS', 'handleError'), E_ALL);
 // register shutdown method
 register_shutdown_function(array('IKARUS', 'destruct'));
 
-// register autoloader
-spl_autoload_register(array('IKARUS', 'autoload'));
+/**
+ * Autoloads classes in namespaces
+ * 
+ * @param		string		$className
+ */
+function autoload($className) {
+	// get globals
+	global $packageList;
+	
+	// little validation ... (this should never happen)
+	if (empty($className)) return;
+	
+	// split to parts
+	$namespaceParts = explode('\\', $className);
+	
+	// find prefix
+	foreach($packageList as $packagePrefix => $packageDir) {
+		if ($namespaceParts[0] == $packagePrefix) {
+			// remove prefix
+			$className = substr($className, (strlen($packagePrefix + 1)));
+			
+			// replace backslashes
+			$className = str_replace('\\', '/', $className);
+			
+			// add '.class.php'
+			$className .= '.class.php';
+			
+			// try to find file and include
+			if (file_exists($packageDir.$className)) require_once($packageDir.$className);
+			
+			// exit the autoloader here. We've unique prefixes.
+			return;
+		}
+	}
+}
+spl_autoload_register('autoload');
 
 /**
  * Escapes a string with the correct method for the current database connection
