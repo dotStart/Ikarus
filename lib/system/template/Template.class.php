f202062cf4c37128fb304b1d2505149bb96b8e5e
Johannes Donath <akkarin@develfusion.com>
2011-05-08 05:52:17 +0200

    Added Template.class.php for smarty (With funny Namespaces ...)

---
+++ lib/system/template/Template.class.php
@@ -0,0 +1,25 @@
+<?php
+namespace ikarus\system\template;
+
+// imports
+require_once(IKARUS_DIR.'lib/system/template/Smarty.class.php');
+
+/**
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @package		com.develfusion.ikarus
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		1.0.0-0001
+ */
+class Template extends \Smarty {
+	
+	/**
+	 * @see lib/system/template/Smarty.class.php::__construct()
+	 */
+	public function __construct() {
+		parent::__construct();
+	}
+}
+?>
