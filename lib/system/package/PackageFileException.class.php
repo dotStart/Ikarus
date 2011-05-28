243048ad5d0009dc39bdfce615342d0b2a7d83cd
Johannes Donath <akkarin@develfusion.com>
2011-05-28 17:29:27 +0200

    Added PackageFileException

---
+++ lib/system/package/PackageFileException.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace ikarus\system\package;
+use ikarus\system\exception\SystemException;
+
+/**
+ * This exception will thrown if an error in an IPF file occours
+ * @author		Johannes Donath
+ * @copyright		2011 Evil-Co.de
+ * @package		com.develfusion.ikarus
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		1.0.0-0001
+ */
+class PackageFileException extends SystemException { }
+?>
