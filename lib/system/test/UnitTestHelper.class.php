92ef01601fd3e05323ebbe2339c9f744a8f40a56
Johannes Donath <akkarin@develfusion.com>
2011-07-02 00:32:15 +0200

    PHPDoc More nice features

--- lib/system/test/UnitTestHelper.class.php
+++ lib/system/test/UnitTestHelper.class.php
@@ -20,6 +20,24 @@ class UnitTestHelper {
 	protected static $instance = null;
 	
 	/**
+	 * Contains a count of succeeded tests
+	 * @var			integer
+	 */
+	protected $succeededTests = 0;
+	
+	/**
+	 * Contains a count of failed tests
+	 * @var			integer
+	 */
+	protected $failedTests = 0;
+	
+	/**
+	 * Contains a count of executed tests
+	 * @var			integer
+	 */
+	protected $executedTests = 0;
+	
+	/**
 	 * Creates a new instance of type UnitTestHelper
 	 */
 	protected function __construct() { }
@@ -31,8 +49,76 @@ class UnitTestHelper {
 	protected function __clone() { }
 	
 	/**
+	 * Executes an assertion for given variable
+	 * @param		string			$message			This message will appear if the test fails
+	 * @param		mixed			$variable
+	 * @throws		SystemException
+	 * @return		void
+	 */
+	public function assert($message, $variable) {
+		// count tests
+		$this->executedTests++;
+		
+		// count succeeded tests
+		if ($variable) {
+			$this->succeededTests++;
+			return;
+		}
+		
+		$this->failedTests++;
+		try {
+			throw new SystemException("Unit test failed: %s", $message);
+		} catch (SystemException $ex) {
+			$ex->show(); // Just a little workaround to get the default SystemException design without killing the whole process
+		}
+	}
+	
+	/**
+	 * Checks for equal variables
+	 * @param		string			$message
+	 * @param		mixed			$variable1
+	 * @param		mixed			$variable2
+	 * @return		void
+	 */
+	public function assertEqual($message, $variable1, $variable2) {
+		$this->assert($message, $variable1 == $variable2);
+	}
+	
+	/**
+	 * Checks for false variables
+	 * @param		string			$message
+	 * @param		mixed			$variable
+	 * @return		void
+	 */
+	public function assertFalse($message, $variable) {
+		$this->assert($message, !(bool) $variable);
+	}
+	
+	/**
+	 * Checks for identical variables
+	 * @param		string			$message
+	 * @param		mixed			$variable1
+	 * @param		mixed			$variable2
+	 * @return		void
+	 */
+	public function assertIdentical($message, $variable1, $variable2) {
+		$this->assert($message, $variable1 === $variable2);
+	}
+	
+	/**
+	 * Checks for true variables
+	 * @param		string			$message
+	 * @param		mixed			$variable
+	 * @return		void
+	 */
+	public function assertTrue($message, $variable) {
+		$this->assert($message, (bool) $variable);
+	}
+	
+	/**
 	 * Executes all tests of given class
 	 * @param		object		$testClass
+	 * @return		void
 	 */
 	public function executeTests($testClass) {
 		// get reflection instance
