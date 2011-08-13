0ff886714a0c635814e51272c30341076833df11
Johannes Donath <akkarin@develfusion.com>
2011-08-13 10:55:19 +0200

    Added a file with core definitions

---
+++ lib/core.defines.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author		Johannes Donath
+ * @copyright		2010 DEVel Fusion
+ * @package		com.develfusion.ikarus
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		2.0.0-0001
+ */
+
+/** version defines **/
+define('IKARUS_VERSION', '2.0.0');
+define('IKARUS_VERSION_MAJOR', 2);
+define('IKARUS_VERSION_MINOR', 0);
+define('IKARUS_VERSION_REVISION', 0);
+define('IKARUS_VERSION_BUILD', '0001');
+?>
