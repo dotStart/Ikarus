f05f0415091aea3c4d7c81604f9e48ceb578502a
Johannes Donath <akkarin@develfusion.com>
2011-08-13 13:21:02 +0200

    Hiding error messages will be implemented later ...

--- lib/system/exception/SystemException.class.php
+++ lib/system/exception/SystemException.class.php
@@ -112,7 +112,7 @@ class SystemException extends Exception implements IPrintableException {
 		// print report
 		echo '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"><html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" xml:lang="en">';
 
-		if (DISPLAY_ERRORS) {
+		if (!defined('HIDE_ERRORS') or !HIDE_ERRORS) {
 			?>
 <head>
 <title>Fatal error: <?php echo StringUtil::encodeHTML($this->getMessage()); ?>
