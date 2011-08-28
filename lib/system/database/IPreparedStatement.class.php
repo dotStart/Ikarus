b698cba2ca5efe04e169ba0e04a7e07ddc1e8734
Johannes Donath <akkarin@develfusion.com>
2011-08-29 00:29:37 +0200

    Created interface for prepared statements

---
+++ lib/system/database/IPreparedStatement.class.php
@@ -0,0 +1,45 @@
+<?php
+namespace ikarus\system\database;
+
+/**
+ * Defines needed methods for prepared statements
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @package		com.develfusion.ikarus
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		2.0.0-0001
+ */
+interface IPreparedStatement {
+	
+	/**
+	 * Creates a new instance of type IPreparedStatement
+	 * @param			ikarus\system\database\adapter\IDatabaseAdapter		$adapter
+	 * @param			string							$statement
+	 */
+	public function __construct(ikarus\system\database\adapter\IDatabaseAdapter $adapter, $statement);
+	
+	/**
+	 * Binds a parameter
+	 * @param			mixed			$value
+	 * @param			integer			$position
+	 * @return			void
+	 */
+	public function bind($value, $position = null);
+	
+	/**
+	 * Binds a named parameter
+	 * @param			string			$name
+	 * @param			mixed			$value
+	 * @return			void
+	 */
+	public function bindNamedParameter($name, $value);
+	
+	/**
+	 * Executes the statement
+	 * @return			mixed
+	 */
+	public function execute();
+}
+?>
