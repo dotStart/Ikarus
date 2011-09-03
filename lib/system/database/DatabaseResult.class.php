95c2b5bccd5c3d62d9fcd0a488289c5598ec6668
Johannes Donath <akkarin@develfusion.com>
2011-09-03 19:47:09 +0200

    Implemented DatabaseResult (Result representation for all database types)

---
+++ lib/system/database/DatabaseResult.class.php
@@ -0,0 +1,30 @@
+<?php
+namespace ikarus\system\database;
+use ikarus\data\DatabaseObject;
+
+/**
+ * Represents a database result
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @package		com.develfusion.ikarus
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		2.0.0-0001
+ */
+class DatabaseResult extends DatabaseObject {
+	
+	/**
+	 * @see ikarus\data.DatabaseObject::handleData()
+	 */
+	protected function handleData($data) {
+		// handle arrays
+		if (is_array($data)) return parent::handleData($data);
+		
+		// handle objects
+		foreach($data as $row) {
+			$this->data[$key] = $value;
+		}
+	}
+}
+?>
