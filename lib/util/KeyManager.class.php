5ddd469df3ec0ca7ebd7330d1d8230050d8f6a5f
Johannes Donath <akkarin@develfusion.com>
2011-09-05 21:07:32 +0200

    Implemented KeyManager

---
+++ lib/util/KeyManager.class.php
@@ -0,0 +1,30 @@
+<?php
+namespace ikarus\util;
+use ikarus\system\exception\KeyException;
+
+/**
+ * Manages all openssl keys used by ikarus
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @package		com.develfusion.ikarus
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		2.0.0-0001
+ */
+class KeyManager {
+	
+	/**
+	 * Returns the maintainer public key (Used to encrypt error reports)
+	 * @throws			KeyException
+	 * @return			string
+	 */
+	public static function getMaintainerKey() {
+		// validate key file
+		if (!file_exists(IKARUS_DIR.'keys/maintainer.pub')) throw new KeyException("Cannot load maintainer key: File does not exist!");
+		
+		// return content
+		return file_get_contents(IKARUS_DIR.'keys/maintainer.pub');
+	}
+}
+?>
