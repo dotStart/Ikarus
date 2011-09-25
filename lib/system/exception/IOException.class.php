c3e5fb7b4aaea9996298b48051746175c46a6fb3
Johannes Donath <akkarin@develfusion.com>
2011-09-25 20:46:15 +0200

    Created IOException

---
+++ lib/system/exception/IOException.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace ikarus\system\exception;
+
+/**
+ * @author		Johannes Donath
+ * @copyright		2011 Evil-Co.de
+ * @package		de.ikarus-framework.core
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		2.0.0-0001
+ */
+class IOException extends SystemException { }
+?>
