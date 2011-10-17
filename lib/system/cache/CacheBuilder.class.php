2841fc6fc753246048afdefe3d9075cd75a731ca
Johannes Donath <akkarin@develfusion.com>
2011-10-17 16:02:06 +0200

    Added CacheBuilder interface

---
+++ lib/system/cache/CacheBuilder.class.php
@@ -0,0 +1,23 @@
+<?php
+namespace ikarus\system\cache;
+
+/**
+ * Defines needed methods for cache builders
+ * @author		Johannes Donath
+ * @copyright		2011 Evil-Co.de
+ * @package		de.ikarus-framework.core
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		2.0.0-0001
+ */
+interface CacheBuilder {
+	
+	/**
+	 * Returns datat that should be written to cache
+	 * @param			string			$resourceName
+	 * @return			mixed
+	 */
+	public static function getData($resourceName);
+}
+?>
