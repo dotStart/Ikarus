d872bd1f2dae8757e9a232ea3c94d14468101de9
Johannes Donath <akkarin@develfusion.com>
2011-10-18 11:11:46 +0200

    Created IkarusApplication as example application

---
+++ lib/system/application/IkarusApplication.class.php
@@ -0,0 +1,26 @@
+<?php
+namespace ikarus\system\application;
+
+/**
+ * Ikarus Example Application Mode
+ * Note: This class also introduces the developer mode
+ * @author		Johannes Donath
+ * @copyright		2011 Evil-Co.de
+ * @package		de.ikarus-framework.core
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		2.0.0-0001
+ */
+class IkarusApplication extends AbstractWebApplication {
+	
+	/**
+	 * @see ikarus\system\application.IApplication::__construct()
+	 * @throws ApplicationException
+	 */
+	public function __construct($abbreviation, $libraryPath, $packageID, $environment) {
+		if ($environment != 'administration') throw new ApplicationException("How did you reach this exception? WTF?!");
+		parent::__construct($abbreviation, $libraryPath, $packageID, $environment);
+	}
+}
+?>
