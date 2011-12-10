1c4daed73085175cc303e05a786ae110a695dc02
Johannes Donath <akkarin@develfusion.com>
2011-12-10 04:48:40 +0100

    Added NamedUserException and IllegalLinkException

---
+++ lib/system/exception/NamedUserException.class.php
@@ -0,0 +1,50 @@
+<?php
+namespace ikarus\system\exception;
+use ikarus\system\Ikarus;
+
+/**
+ * @author		Johannes Donath
+ * @copyright		2011 Evil-Co.de
+ * @package		de.ikarus-framework.core
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		2.0.0-0001
+ */
+class NamedUserException extends SystemException {
+	
+	/**
+	 * Contains a HTTP header that should be used
+	 * @var			string
+	 */
+	protected $header = 'HTTP/1.1 200 Ok';
+	
+	/**
+	 * Contains the name of a template that should appear
+	 * @var			string
+	 */
+	protected $template = 'namedUserException';
+	
+	/**
+	 * Creates a new instance of type SystemException
+	 * @param			string			$message
+	 */
+	public function __construct($message = '') {
+		$this->message = $message;
+	}
+	
+	/**
+	 * @see ikarus\system\exception.SystemException::show()
+	 */
+	public function show() {
+		// template system
+		if (Ikarus::componentLoaded('Template') and !empty($this->template)) {
+			Ikarus::getComponent('Template')->assign('message', $this->message);
+			Ikarus::getComponent('Template')->display($this->template);
+		}
+		
+		// no template system
+		parent::show();
+	}
+}
+?>
