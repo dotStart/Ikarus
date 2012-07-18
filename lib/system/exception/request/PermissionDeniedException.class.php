a3c935e5509dd3771bd697726c9c134df3e55af7
Johannes Donath <akkarin@develfusion.com>
2012-07-18 07:21:57 +0200

    Fixed header of ikarus\system\exception\request.PermissionDeniedException

--- lib/system/exception/request/PermissionDeniedException.class.php
+++ lib/system/exception/request/PermissionDeniedException.class.php
@@ -16,6 +16,6 @@ class PermissionDeniedException extends AccessDeniedException {
 	/**
 	 * @see ikarus\system\exception.NamedUserException::$header
 	 */
-	protected $header = 'HTTP/1.1 401 Unauthorized';
+	protected $header = 'HTTP/1.1 403 Forbidden';
 }
 ?>
