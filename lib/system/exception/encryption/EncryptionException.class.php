67387cf9632f6a0df8d3e444c10effab02e2fbb1
Johannes Donath <akkarin@develfusion.com>
2012-07-18 06:58:09 +0200

    Created ikarus\system\exception\encryption.EncryptionException

---
+++ lib/system/exception/encryption/EncryptionException.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace ikarus\system\exception\encryption;
+use ikarus\system\exception\SystemException;
+
+/**
+ * The base class for all encryption problems.
+ * @author		Johannes Donath
+ * @copyright		2012 Evil-Co.de
+ * @package		de.ikarus-framework.core
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		2.0.0-0001
+ */
+class EncryptionException extends SystemException { }
+?>
