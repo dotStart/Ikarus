39a4111a47497911d7ed4e58b7a5909b89b99684
Johannes Donath <akkarin@develfusion.com>
2012-07-18 07:17:53 +0200

    Added ikarus\system\exception\request.PermissionDeniedException

---
+++ lib/system/exception/request/PermissionDeniedException.class.php
@@ -0,0 +1,21 @@
+<?php
+namespace ikarus\system\exception\request;
+
+/**
+ * This exception will be thrown if more permissions are required to access the resource.
+ * @author		Johannes Donath
+ * @copyright		2012 Evil-Co.de
+ * @package		de.ikarus-framework.core
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		2.0.0-0001
+ */
+class PermissionDeniedException extends AccessDeniedException {
+	
+	/**
+	 * @see ikarus\system\exception.NamedUserException::$header
+	 */
+	protected $header = 'HTTP/1.1 401 Unauthorized';
+}
+?>
