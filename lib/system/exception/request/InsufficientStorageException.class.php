106db80db09aa5d48823b3ed3a0e0c509f32e5f3
Johannes Donath <akkarin@develfusion.com>
2012-07-18 09:37:22 +0200

    Added ikarus\system\exception\request.InsufficientStorageException

---
+++ lib/system/exception/request/InsufficientStorageException.class.php
@@ -0,0 +1,21 @@
+<?php
+namespace ikarus\system\exception\request;
+
+/**
+ * This exception will be thrown if there's not enough storage available to store a file.
+ * @author		Johannes Donath
+ * @copyright		2012 Evil-Co.de
+ * @package		de.ikarus-framework.core
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		2.0.0-0001
+ */
+class InsufficientStorageException extends RequestException {
+	
+	/**
+	 * @see ikarus\system\exception.NamedUserException::$header
+	 */
+	protected $header = 'HTTP/1.1 507 Insufficient Storage';
+}
+?>
