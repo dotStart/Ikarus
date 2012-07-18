f462508087c4c5f00583423e6678bb047a4d5c1f
Johannes Donath <akkarin@develfusion.com>
2012-07-18 07:00:59 +0200

    Created ikarus\system\exception\encryption.EncryptionFailedException

---
+++ lib/system/exception/encryption/EncryptionFailedException.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace ikarus\system\exception\encryption;
+
+/**
+ * This exception will be thrown if an encryption fails for some reason.
+ * @author		Johannes Donath
+ * @copyright		2012 Evil-Co.de
+ * @package		de.ikarus-framework.core
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		2.0.0-0001
+ */
+class EncryptionFailedException extends EncryptionException { }
+?>
