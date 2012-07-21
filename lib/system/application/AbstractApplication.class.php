1dee8d547aa433c2881c70add0792196860d69f7
Johannes Donath <akkarin@develfusion.com>
2012-07-21 08:23:16 +0200

    Removed whitespaces

--- lib/system/application/AbstractApplication.class.php
+++ lib/system/application/AbstractApplication.class.php
@@ -35,43 +35,43 @@ use ikarus\util\ClassUtil;
  * @version		2.0.0-0001
  */
 abstract class AbstractApplication implements IApplication {
-	
+
 	/**
 	 * Contains the abbreviation for this application
 	 * @var			string
 	 */
 	protected $abbreviation = '';
-	
+
 	/**
 	 * Contains all application components
 	 * @var			array
 	 */
 	protected $components = array();
-	
+
 	/**
 	 * Contains the choosen environment
 	 * @var			string
 	 */
 	protected $environment = '';
-	
+
 	/**
 	 * Contains an library namespace for this application
 	 * @var			string
 	 */
 	protected $libraryNamespace = '';
-	
+
 	/**
 	 * Contains the packageID of this application
 	 * @var			integer
 	 */
 	protected $packageID = 0;
-	
+
 	/**
 	 * Contains true if this application is the primary application of this framework instance
 	 * @var			boolean
 	 */
 	protected $primaryApplication = false;
-	
+
 	/**
 	 * @see ikarus\system\application.IApplication::__construct()
 	 */
@@ -81,77 +81,77 @@ abstract class AbstractApplication implements IApplication {
 		$this->packageID = $packageID;
 		$this->environment = $environment;
 		$this->primaryApplication = $primaryApplication;
-		
+
 		// fire InitFinished event
 		Ikarus::getEventManager()->fire(new InitFinishedEvent(new ApplicationEventArguments($this)));
 	}
-	
+
 	/**
 	 * @see ikarus\system\application.IApplication::boot()
 	 */
 	public function boot() {
 		Ikarus::getEventManager()->fire(new BootEvent(new ApplicationEventArguments($this)));
-		
+
 		$this->registerDefaultCacheResources();
 		$this->registerDefaultComponents();
 	}
-	
+
 	/**
 	 * @see ikarus\system\application.IApplication::getAbbreviation()
 	 */
 	public function getAbbreviation() {
 		return $this->abbreviation;
 	}
-	
+
 	/**
 	 * @see ikarus\system\application.IApplication::getEnvironment()
 	 */
 	public function getEnvironment() {
 		return $this->environment;
 	}
-	
+
 	/**
 	 * @see ikarus\system\application.IApplication::getLibraryNamespace()
 	 */
 	public function getLibraryNamespace() {
 		return $this->libraryNamespace;
 	}
-	
+
 	/**
 	 * @see ikarus\system\application.IApplication::getPackageID()
 	 */
 	public function getPackageID() {
 		return $this->packageID;
 	}
-	
+
 	/**
 	 * @see ikarus\system\application.IApplication::isPrimaryApplication()
 	 */
 	public function isPrimaryApplication() {
 		return $this->primaryApplication;
 	}
-	
+
 	/**
 	 * Registers all default cache resources for this application
 	 * @return			void
 	 */
 	protected function registerDefaultCacheResources() { }
-	
+
 	/**
 	 * Registers all default components for this application
 	 * @return			void
 	 */
 	protected function registerDefaultComponents() { }
-	
+
 	/**
 	 * @see ikarus\system\application.IApplication::shutdown()
 	 */
 	public function shutdown() {
 		Ikarus::getEventManager()->fire(new ShutdownEvent(new ApplicationEventArguments($this)));
-		
+
 		$this->shutdownDefaultComponents();
 	}
-	
+
 	/**
 	 * @see ikarus\system\application.IApplication::shutdownDefaultComponents()
 	 */
