a7921827f6261044470b0ea0f77f46a2b2f4dab1
Johannes Donath <akkarin@develfusion.com>
2012-07-26 14:59:35 +0200

    Added support for ikarus\system\event\session\factory.CreateSessionEvent

--- lib/system/session/SessionFactory.class.php
+++ lib/system/session/SessionFactory.class.php
@@ -17,11 +17,11 @@
  */
 namespace ikarus\system\session;
 use ikarus\system\exception\StrictStandardException;
+use ikarus\system\event\session\CreatedSessionIDEvent;
+use ikarus\system\event\session\factory\SessionIDArguments;
 use ikarus\system\Ikarus;
 use ikarus\util\ClassUtil;
 use ikarus\util\StringUtil;
-
-use ikarus\util\DebugUtil;
 
 /**
  * Provides methods for creating new sessions
@@ -34,13 +34,13 @@ use ikarus\util\DebugUtil;
  * @version		2.0.0-0001
  */
 class SessionFactory {
-	
+
 	/**
 	 * Contains the namespace path that points to custom session classes
 	 * @var			string
 	 */
 	const SESSION_CLASS_PATH = 'system\\session\\Session';
-	
+
 	/**
 	 * Creates a new session with the specified abbreviation for the specified application
 	 * @param			string			$sessionID
@@ -52,19 +52,28 @@ class SessionFactory {
 	public static function createSession($sessionID, $abbreviation, $packageID, $environment) {
 		// no custom session class? exit!
 		if (!class_exists($abbreviation.'\\'.static::SESSION_CLASS_PATH)) return false;
-		
+
 		// create session instance
 		$className = $abbreviation.'\\'.static::SESSION_CLASS_PATH;
-		
+
 		$instance = new $className(array(
 			'sessionID'			=>	$sessionID,
 			'packageID'			=>	$packageID,
 			'environment'			=>	$environment
 		));
-		
+
+		// fire event
+		$event = new CreateSessionEvent(new SessionEventArguments($instance));
+		Ikarus::getEventManager()->fire($event);
+
+		if ($event->isCancelled() and $event->getReplacement() === null)
+			throw new MissingDependencyException('No replacement has been supplied to %s', 'CreateSessionEvent');
+		elseif ($event->isCancelled())
+			$instance = $event->getReplacement();
+
 		// strict standards
 		if (!ClassUtil::isInstanceOf($instance, 'ikarus\\system\\session\\ISession')) throw new StrictStandardException('Session class \'%s\' is not an instance of ikarus\\system\\session\\ISession', $className);
-		
+
 		// save
 		$sql = "INSERT INTO
 				ikarus".IKARUS_N."_session (sessionID, sessionData, ipAddress, userAgent, packageID, environment, abbreviation)
@@ -79,13 +88,13 @@ class SessionFactory {
 		$stmt->bind($environment);
 		$stmt->bind($abbreviation);
 		$stmt->execute();
-		
+
 		// update session
 		$instance->update();
-		
+
 		Ikarus::getComponent('SessionManager')->registerSession($abbreviation, $instance);
 	}
-	
+
 	/**
 	 * Creates a new session ID
 	 * @todo			Queries in while loops are not the best way ...
@@ -103,7 +112,10 @@ class SessionFactory {
 			$stmt = Ikarus::getDatabaseManager()->getDefaultAdapter()->prepareStatement($sql);
 			$stmt->bind($sessionID);
 		} while (count($stmt->fetchList()) > 0);
-		
+
+		// fire event
+		Ikarus::getEventManager()->fire(new CreatedSessionIDEvent(new SessionIDArguments($sessionID)));
+
 		return $sessionID;
 	}
 }
