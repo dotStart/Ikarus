efb194906097ab87f0280463b4b7f88d46a3f13f
Johannes Donath <akkarin@develfusion.com>
2012-07-28 17:36:01 +0200

    Added events ...

--- lib/system/request/RequestDispatcher.class.php
+++ lib/system/request/RequestDispatcher.class.php
@@ -17,6 +17,12 @@
  */
 namespace ikarus\system\request;
 use ikarus\pattern\Singleton;
+use ikarus\system\application\IApplication;
+use ikarus\system\event\request\ControllerEventArguments;
+use ikarus\system\event\request\ExecuteControllerEvent;
+use ikarus\system\event\request\DispatcherEventArguments;
+use ikarus\system\event\request\DispatcherFailedEvent;
+use ikarus\system\event\request\DispatchEvent;
 use ikarus\system\exception\request\IllegalLinkException;
 use ikarus\system\Ikarus;
 use ikarus\util\ArrayUtil;
@@ -34,53 +40,68 @@ use ikarus\util\FileUtil;
  * @version		2.0.0-0001
  */
 class RequestDispatcher extends Singleton {
-	
+
 	/**
 	 * Contains a list of available controller types
 	 * @var			array<array<string>>
 	 */
 	protected $availableControllerTypes = array();
-	
+
 	/**
 	 * Contains a list of available routes
 	 * @var			array<string>
 	 */
 	protected $availableRoutes = array();
-	
+
 	/**
 	 * @see ikarus\pattern.Singleton::__construct()
 	 */
 	protected function __construct($packageID = IKARUS_N) {
 		parent::__construct();
-		
+
 		$this->loadDispatcherCache($packageID);
 	}
-	
+
 	/**
 	 * Dispatches a request to correct controller
 	 * @param			IApplication		$application
 	 * @param			string			$requestParameters
 	 * @throws IllegalLinkException
 	 */
-	public function dispatch($application, $requestParameters) {
+	public function dispatch(IApplication $application, $requestParameters) {
 		// set defaults
 		// FIXME: This should not be hardcoded
 		if (!ArrayUtil::in_array(array_keys($requestParameters), $this->availableControllerTypes)) $requestParameters['page'] = 'Index';
-		
+
+		// fire event
+		$event = new DispatchEvent(new DispatcherEventArguments($application, $requestParameters));
+		Ikarus::getEventManager()->fire($event);
+
+		// cancellable event
+		if ($event->isCancelled()) return;
+
 		// search for routes
 		// Note: This allows shadowing existing static routes (Defined by files)
 		foreach($this->availableRoutes as $routeParameter => $routes)
 			if (isset($requestParameters[$routeParameter]))
 				foreach($routes as $routeName => $executionInformation)
 					if ($routeName == $requestParameters[$routeParameter] and $this->loadController($executionInformation['controllerName'], $executionInformation['controllerNamespace'], $application)) return $this->executeController($executionInformation['controllerName'], $executionInformation['controllerNamespace'], $application);
-		
+
 		// find controller types
 		foreach($this->availableControllerTypes as $name => $controllerNamespace)
 			if (isset($requestParameters[$name]) and $this->loadController($requestParameters[$name].ucfirst($name), $controllerNamespace, $application)) return $this->executeController($requestParameters['name'], $controllerNamespace, $application);
-		
+
+		// fire event
+		$event = new DispatcherFailedEvent(new DispatcherEventArguments($application, $requestParameters));
+		Ikarus::getEventManager()->fire($event);
+
+		// cancellable event
+		if ($event->isCancelled()) return;
+
+		// no routes found
 		throw new IllegalLinkException('There are no routes and no controllers available for this request.');
 	}
-	
+
 	/**
 	 * Executes a controller.
 	 * @param			string			$controllerName
@@ -90,14 +111,24 @@ class RequestDispatcher extends Singleton {
 	public function executeController($controllerName, $controllerNamespace, $application) {
 		// build classPath
 		$classPath = ClassUtil::buildPath($application->getLibraryNamespace(), $controllerNamespace, $controllerName);
-		
+
 		// create controller instance
 		$controller = new $classPath();
-		
+
+		// fire event
+		$event = new ExecuteControllerEvent(new ControllerEventArguments($controller));
+		Ikarus::getEventManager()->fire($event);
+
+		// cancellable event
+		if ($event->isCancelled() and $event->getReplacement() === null)
+			throw new StrictStandardException('A replacement for controller execution is missing');
+		elseif ($event->isCancelled())
+			$controller = $event->getReplacement();
+
 		// execute controller
 		$controller->init();
 	}
-	
+
 	/**
 	 * Loads a controller.
 	 * @param		string			$controllerName
@@ -107,11 +138,11 @@ class RequestDispatcher extends Singleton {
 	public function loadController($controllerName, $controllerNamespace, $application) {
 		// build classPath
 		$classPath = ClassUtil::buildPath($application->getLibraryNamespace(), $controllerNamespace, $controllerName);
-		
+
 		// try to load controller
 		return class_exists($classPath, true);
 	}
-	
+
 	/**
 	 * Loads all available dispatcher caches
 	 * @return			void
@@ -120,7 +151,7 @@ class RequestDispatcher extends Singleton {
 		// add resources
 		Ikarus::getCacheManager()->getDefaultAdapter()->createResource('requestDispatcherControllerTypes-'.$packageID, 'requestDispatcherControllerTypes-'.$packageID, 'ikarus\system\cache\builder\CacheBuilderRequestDispatcherControllerTypes');
 		Ikarus::getCacheManager()->getDefaultAdapter()->createResource('requestDispatcherAvailableRoutes-'.$packageID, 'requestDispatcherAvailableRoutes-'.$packageID, 'ikarus\system\cache\builder\CacheBuilderRequestDispatcherAvailableRoutes');
-		
+
 		// save information
 		$this->availableControllerTypes = Ikarus::getCacheManager()->getDefaultAdapter()->get('requestDispatcherControllerTypes-'.$packageID);
 		$this->availableRoutes = Ikarus::getCacheManager()->getDefaultAdapter()->get('requestDispatcherAvailableRoutes-'.$packageID);
