cbd8f8505b968b8bcc8ee54aebf36058b333342d
Johannes Donath <akkarin@develfusion.com>
2012-07-28 11:30:12 +0200

    Added events

--- lib/system/style/StyleManager.class.php
+++ lib/system/style/StyleManager.class.php
@@ -18,11 +18,12 @@
 namespace ikarus\system\style;
 use ikarus\system\application\IConfigurableComponent;
 use ikarus\system\application\IApplication;
+use ikarus\system\event\DefaultStyleSetEvent;
 use ikarus\system\exception\SystemException;
 use ikarus\system\Ikarus;
 
 /**
- * 
+ *
  * @author		Johannes Donath
  * @copyright		2011 Evil-Co.de
  * @package		de.ikarus-framework.core
@@ -32,25 +33,25 @@ use ikarus\system\Ikarus;
  * @version		2.0.0-0001
  */
 class StyleManager implements IConfigurableComponent {
-	
+
 	/**
 	 * Contains the current activated style
 	 * @var			ikarus\system\style\Style
 	 */
 	protected $activeStyle = null;
-	
+
 	/**
 	 * Contains the active application
 	 * @var			ikarus\system\application\IApplication
 	 */
 	protected $application = null;
-	
+
 	/**
 	 * Contains a cached list of styles
 	 * @var			array<ikarus\system\style\Style>
 	 */
 	protected $styleList = array();
-	
+
 	/**
 	 * Configures the style instance
 	 * @param			ikarus\system\application\IApplication			$application
@@ -58,14 +59,14 @@ class StyleManager implements IConfigurableComponent {
 	 */
 	public function configure(IApplication $application) {
 		$this->application = $application;
-		
+
 		// load cache
 		$this->loadStyleCache();
-		
+
 		// set active style
 		$this->setActiveStyle(((isset($_REQUEST['styleID']) and $this->getStyle($_REQUEST['styleID']) !== null) ? $this->getStyle($_REQUEST['styleID']) : $this->getDefaultStyle()));
 	}
-	
+
 	/**
 	 * Returns the current active style
 	 * @return			ikarus\system\style\Style
@@ -73,7 +74,7 @@ class StyleManager implements IConfigurableComponent {
 	public function getActiveStyle() {
 		return $this->activeStyle;
 	}
-	
+
 	/**
 	 * Returns the default style
 	 * @throws			ikarus\system\exception\SystemException
@@ -83,7 +84,7 @@ class StyleManager implements IConfigurableComponent {
 		foreach($this->styleList as $style) if ($style->isDefault) return $style;
 		throw new SystemException('No default style set');
 	}
-	
+
 	/**
 	 * Returns a style by identifier
 	 * @param			integer			$styleID
@@ -93,23 +94,30 @@ class StyleManager implements IConfigurableComponent {
 		foreach($this->styleList as $style) if ($style->styleID == $styleID) return $style;
 		return null;
 	}
-	
+
 	/**
 	 * Loads all style manager caches
 	 * @return			void
 	 */
 	protected function loadStyleCache() {
 		Ikarus::getCacheManager()->getDefaultAdapter()->createResource('styleList-'.$this->application->getPackageID().'-'.$this->application->getEnvironment(), 'styleList-'.$this->application->getPackageID().'-'.$this->application->getEnvironment(), 'ikarus\system\cache\builder\CacheBuilderStyleList');
-	
+
 		$this->styleList = Ikarus::getCacheManager()->getDefaultAdapter()->get('styleList-'.$this->application->getPackageID().'-'.$this->application->getEnvironment());
 	}
-	
+
 	/**
 	 * Activates a style
 	 * @param			ikarus\system\style\Style			$style
 	 * @return			void
 	 */
 	public function setActiveStyle(Style $style) {
+		// fire event
+		$event = new DefaultStyleSetEvent(new StyleEventArguments($style));
+		Ikarus::getEventManager()->fire($event);
+
+		// cancellable event
+		if ($event->isCancelled()) return;
+
 		$this->activeStyle = $style;
 		$this->activeStyle->loadCache();
 	}
