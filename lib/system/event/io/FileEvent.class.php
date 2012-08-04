3e436788b5eb2b03734cac69f56f7acc0af75134
Johannes Donath <akkarin@develfusion.com>
2012-08-04 13:24:10 +0200

    Fixed use flags

--- lib/system/event/io/FileEvent.class.php
+++ lib/system/event/io/FileEvent.class.php
@@ -16,6 +16,7 @@
  * along with the Ikarus Framework. If not, see <http://www.gnu.org/licenses/>.
  */
 namespace ikarus\system\event\io;
+use ikarus\system\event\AbstractEvent;
 
 /**
  * This is the base file for all file related events.
