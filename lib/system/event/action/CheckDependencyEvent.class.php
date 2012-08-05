ee9ac1540f0d0d5c7d62868a77dc7aa4fa52f9d0
Johannes Donath <akkarin@develfusion.com>
2012-08-05 23:10:33 +0200

    PHPDoc

--- lib/system/event/action/CheckDependencyEvent.class.php
+++ lib/system/event/action/CheckDependencyEvent.class.php
@@ -17,5 +17,15 @@
  */
 namespace ikarus\system\event\action;
 
+/**
+ * This event occurs if the dependencies are checked.
+ * @author		Johannes Donath
+ * @copyright		2012 Evil-Co.de
+ * @package		de.ikarus-framework.core
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		2.0.0-0001
+ */
 class CheckDependencyEvent extends CancellableActionEvent { }
 ?>
