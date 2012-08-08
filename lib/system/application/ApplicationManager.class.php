bc614e32e2c3381b373ee421b758492a424f290e
Johannes Donath <akkarin@develfusion.com>
2012-08-08 15:52:28 +0200

    Fixed application system

--- lib/system/application/ApplicationManager.class.php
+++ lib/system/application/ApplicationManager.class.php
@@ -34,20 +34,20 @@ use ikarus\util\FileUtil;
  * @version		2.0.0-0001
  */
 class ApplicationManager {
-	
+
 	/**
 	 * Contains a list of applications
 	 * @var			array<Application>
 	 */
 	protected $applications = array();
-	
+
 	/**
 	 * Creates a new instance of type ApplicationManager
 	 */
 	public function __construct() {
 		$this->loadApplicationCache();
 	}
-	
+
 	/**
 	 * Returns true if the given application abbreviation exists
 	 * @param			string			$abbreviation
@@ -56,7 +56,7 @@ class ApplicationManager {
 	public function applicationAbbreviationExists($abbreviation) {
 		return array_key_exists($abbreviation, $this->applications);
 	}
-	
+
 	/**
 	 * Alias for ApplicationManager::applicationAbbreviationExists()
 	 * @see ikarus\system\application.ApplicationManager.applicationAbbreviationExists()
@@ -64,19 +64,19 @@ class ApplicationManager {
 	public function applicationPrefixExists($prefix) {
 		return $this->applicationAbbreviationExists($prefix);
 	}
-	
+
 	/**
 	 * Boots all applications
 	 * @return			void
 	 */
 	public function boot() {
 		if (!count($this->applications)) throw new SystemException("What the fuck? There are no applications! What are you doing?!");
-		
+
 		foreach($this->applications as $app) {
 			$app->boot();
 		}
 	}
-	
+
 	/**
 	 * Displays the given exception
 	 * @param			SystemException			$ex
@@ -86,7 +86,7 @@ class ApplicationManager {
 		$exception = new HiddenApplicationException($ex->getMessage(), $ex->getCode(), $ex);
 		$exception->show();
 	}
-	
+
 	/**
 	 * Returns the application with given abbreviation
 	 * @param			string			$abbreviation
@@ -96,10 +96,10 @@ class ApplicationManager {
 	public function getApplication($abbreviation) {
 		// strict standard
 		if (!$this->applicationAbbreviationExists($abbreviation)) throw new StrictStandardException("The application abbreviation '%s' does not exist.", $abbreviation);
-		
+
 		return $this->applications[$abbreviation];
 	}
-	
+
 	/**
 	 * Returns a list of all loaded applications
 	 * @return			array<ikarus\system\application\IApplication>
@@ -107,7 +107,7 @@ class ApplicationManager {
 	public function getApplicationList() {
 		return $this->applications;
 	}
-	
+
 	/**
 	 * Handles application errors
 	 * @param			PrintableException			$ex
@@ -117,8 +117,8 @@ class ApplicationManager {
 		if (ClassUtil::isInstanceOf($ex, 'ikarus\\system\\exception\\NamedUserException')) return $ex->show();
 		if (get_class($ex) == 'ikarus\\system\\exception\\SystemException' or Ikarus::getConfiguration()->get('global.advanced.debug') or Ikarus::getConfiguration()->get('global.advanced.showErrors')) return $ex->showMinimal();
 		$this->displayErrorMessage($ex);
-	} 
-	
+	}
+
 	/**
 	 * Loads the application cache
 	 * @return			void
@@ -126,33 +126,30 @@ class ApplicationManager {
 	protected function loadApplicationCache() {
 		// handle developer mode
 		$packageID = (defined('PACKAGE_ID') ? PACKAGE_ID : IKARUS_ID);
-		
+
 		// load cache
 		Ikarus::getCacheManager()->getDefaultAdapter()->createResource('applications-'.$packageID, 'applications-'.$packageID, 'ikarus\system\cache\builder\CacheBuilderApplications');
 		$applicationList = Ikarus::getCacheManager()->getDefaultAdapter()->get('applications-'.$packageID);
-		
+
 		// create application instances
 		foreach($applicationList as $application) {
 			// get class name
 			$className = $application->className;
-			
-			// fix library path
-			$application->libraryPath = FileUtil::getRealPath(IKARUS_DIR.$application->libraryPath);
-			
+
 			// !!!DIRTY FIX!!! We have to load the application core via require_once! This is not the correct way but the library path is not recognized!
-			$includePath = $application->libraryPath.str_replace('\\', '/', substr($application->className, (stripos($application->className, '\\') + 1))).'.class.php';
+			$includePath = IKARUS_DIR.($application->relativeApplicationPath !== null ? $application->relativeApplicationPath : '.').'/lib/'.str_replace('\\', '/', substr($application->className, (stripos($application->className, '\\') + 1))).'.class.php';
 			if (!file_exists($includePath)) throw new ApplicationException("Cannot read application core file '%s': No such file or directory", $includePath);
 			require_once($includePath);
-			
+
 			// check parameters
 			if (!class_exists($className, true)) throw new StrictStandardException("Cannot load application '%s' (%s): Class '%s' was not found", $application->applicationTitle, $application->applicationAbbreviation, $className);
 			if (!ClassUtil::isInstanceOf($className, 'ikarus\\system\\application\\IApplication')) throw new StrictStandardException("Cannot load application '%s' (%s): Class '%s' is not an instance of IApplication", $application->applicationTitle, $application->applicationAbbreviation, $className);
-			
+
 			// create application instance
-			$this->applications[$application->applicationAbbreviation] = new $className($application->applicationAbbreviation, $application->libraryPath, $application->packageID, (defined('IKARUS_ENVIRONMENT') ? IKARUS_ENVIRONMENT : 'frontend'), ($packageID == $application->packageID ? true : false));
+			$this->applications[$application->applicationAbbreviation] = new $className($application->applicationAbbreviation, $application->libraryNamespace, $application->packageID, (defined('IKARUS_ENVIRONMENT') ? IKARUS_ENVIRONMENT : 'frontend'), ($packageID == $application->packageID ? true : false));
 		}
 	}
-	
+
 	/**
 	 * Calls the shutdown method of each active application
 	 * @return			void
