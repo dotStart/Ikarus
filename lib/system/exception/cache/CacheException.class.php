71150f7312468c119c04f66e72dbd37a573f13db
Johannes Donath <akkarin@develfusion.com>
2012-09-02 13:07:58 +0200

    Fixed use flag

--- lib/system/exception/cache/CacheException.class.php
+++ lib/system/exception/cache/CacheException.class.php
@@ -16,6 +16,7 @@
  * along with the Ikarus Framework. If not, see <http://www.gnu.org/licenses/>.
  */
 namespace ikarus\system\exception\cache;
+use ikarus\system\exception\SystemException;
 
 /**
  * Base class for all cache exceptions.
