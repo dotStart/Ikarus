ee51463a53d060bc6cabd77e57c999c76dc77a13
Johannes Donath <akkarin@develfusion.com>
2012-09-26 10:51:05 +0200

    Created IteratorOutOfBoundsException

---
+++ lib/system/exception/iterator/IteratorOutOfBoundsException.class.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file is part of the Ikarus Framework.
+ *
+ * The Ikarus Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Ikarus Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Ikarus Framework. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace ikarus\system\exception\iterator;
+
+/**
+ * This exception occurs if a pointer gets out of bounds.
+ * @author		Johannes Donath
+ * @copyright		2012 Evil-Co.de
+ * @package		de.ikarus-framework.core
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		2.0.0-0001
+ */
+class IteratorOutOfBoundsException extends IteratorException {
+	
+	/**
+	 * Constructs the object.
+	 */
+	public function __construct() {
+		parent::__construct("Pointer out of bounds");
+	}
+}
+?>
