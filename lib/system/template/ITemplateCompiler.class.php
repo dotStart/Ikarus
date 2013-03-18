5939e646b254791aae25aa389e9c7dd5eabd253f
Johannes Donath <akkarin@evil-co.com>
2013-03-18 12:43:54 +0100

    Created template compiler interface

---
+++ lib/system/template/ITemplateCompiler.class.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * This file is part of the Ikarus Framework.
+ *
+ * The Ikarus Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Ikarus Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Ikarus Framework. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace ikarus\system\template;
+
+/**
+ * Defines default methods for template compilers.
+ * @author		Johannes Donath
+ * @copyright		© Copyright 2012 Evil-Co.de <http://www.evil-co.com>
+ * @package		de.ikarus-framework.core
+ * @subpackage		system
+ * @category		Ikarus Framework
+ * @license		GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version		2.0.0-0001
+ */
+interface ITemplateCompiler {
+	
+	/**
+	 * Prepares a template.
+	 * @param			TemplateContext			$context
+	 * @return			CompiledTemplate
+	 */
+	public function compile(TemplateContext $context);
+	
+	/**
+	 * Executes a template.
+	 * @param			CompiledTemplate		$template
+	 * @return			OutputContext
+	 */
+	public function execute(CompiledTemplate $template);
+	
+	/**
+	 * Executes a plugin.
+	 * @param			TemplatePlugin			$plugin
+	 * @param			mixed[]				$arguments
+	 * @param			TemplateContext			$context
+	 * @return			mixed
+	 */
+	public function executePlugin(TemplatePlugin $plugin, array $arguments, TemplateContext $context);
+}
+?>
