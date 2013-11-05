bd8fa8aabfc0b948fb43b2359a79902e99378b4d
Johannes Donath <johannesd@evil-co.com>
2013-11-05 06:54:51 +0100

    Added definition for global context.

---
+++ lib/system/template/IGlobalContext.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of the Ikarus Framework.
+ * The Ikarus Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * The Ikarus Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Ikarus Framework. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace ikarus\system\template;
+
+/**
+ * Class IGlobalContext
+ * @author			Johannes Donath
+ * @copyright			Copyright (C) 2013 Evil-Co <http://www.evil-co.com>
+ * @package			ikarus\system\template
+ * @category			Ikarus Framework
+ * @license			GNU Lesser Public License <http://www.gnu.org/licenses/lgpl.txt>
+ * @version			2.0.0-0001
+ */
+interface IGlobalContext extends ITemplateContext { }
+?> 
